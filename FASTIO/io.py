28b2ed337211332e123b2f928b6068a1f67df10d
Elysia <11175449+jc2915448593@user.noreply.gitee.com>
2025-02-25 14:21:04 +0800

    2025/2/25修改io

--- FASTIO/io.py
+++ FASTIO/io.py
@@ -1,20 +1,14 @@
 import sys
-import os
-
-# import time
 from bisect import bisect_left, bisect_right
-# import functools
 from math import ceil, floor, gcd, factorial, sqrt, log2, log
-import random
-# import re
 from collections import Counter, defaultdict, deque
 # from copy import deepcopy
-from functools import cmp_to_key, lru_cache, reduce
-from heapq import heapify, heappop, heappush, heappushpop, nlargest, nsmallest
+from functools import lru_cache, reduce
+from heapq import heappop, heappush
 from itertools import accumulate, combinations, permutations
-from operator import add, iand, ior, itemgetter, mul, xor
+from operator import add, iand, ior, mul, xor
 from string import ascii_lowercase, ascii_uppercase
-from typing import *
+
 
 input = lambda: sys.stdin.readline().rstrip("\r\n")
 
