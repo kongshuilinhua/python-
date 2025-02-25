690ebb37c3658bea0f9f1902e35942681a29f0d3
Elysia <2915448593@qq.com>
2025-02-25 14:14:24 +0800

    2025/2/25

--- python/蒜法板子/前缀和&差分/求点到其他点的距离和.py
+++ python/蒜法板子/前缀和&差分/求点到其他点的距离和.py
@@ -8,4 +8,5 @@ def getSumAbsoluteDifferences(self, nums: List[int]) -> List[int]:
     res = [0] * n          # res[i] = 前面的个数 * cur - 前面总和 + 后面的和 - 后面个数 * cur
     for i, x in enumerate(nums):
         res[i] = (i + 1) * x - s[i] + (s[n - 1] - s[i]) - (n - i - 1) * x
+        # 2(i+1)x-2s[i]+s[n-1]-nx
     return res
